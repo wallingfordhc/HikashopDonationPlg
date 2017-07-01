6be9834614db769e48409a7a024d140322a3ec8c
David Shannon <dzshanno@yahoo.com>
2017-07-01 20:55:43 +0100

    Update donation2.php

--- donation2.php
+++ donation2.php
@@ -20,7 +20,7 @@ function onBeforeCartUpdate(&$cartClass,&$cart,$product_id,$quantity,$add,$type,
 	
 $cart2      = $cartClass->get();
 if ($cart2) {
-    $numberOfProducts = array_keys($cart2);
+    $numberOfProducts = count($cart2);
     //for ($i = 0; $i < $numberOfProducts; $i++) {
     //    $productID[$i] = $cart2[$i]->product_id;
     //}
